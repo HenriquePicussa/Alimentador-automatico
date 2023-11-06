8ac391754b89393ec37026939972e8a3d491083d
HenriquePicussa <117190737+HenriquePicussa@users.noreply.github.com>
2023-11-05 23:15:46 -0300

    Update login.php

--- Site Pronto/php/login.php
+++ Site Pronto/php/login.php
@@ -3,7 +3,7 @@
     $email = $_POST['email'];
     $senha = $_POST['senha'];
 
-    $con= mysqli_connect('localhost:3306', 'root', 'He@@2037', 'alimentador');
+    $con= mysqli_connect('localhost', '', '', '');
 
     $query = "INSERT INTO entrada (nome, email, senha) VALUES ('$nome', '$email', '$senha')";
 
